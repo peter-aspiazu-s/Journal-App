93bdfdbe4978f130890a08e8a74eaee6d3dcf939
Peter Aspiazu <paspiazusabando@gmail.com>
2022-03-30 09:57:45 -0500

    Actualizando registro y login

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -11,10 +11,10 @@ export const RegisterScreen = () => {
     const {msgError} = useSelector( state => state.ui );
 
     const [ formValues, handleInputChange ] = useForm({
-        name: 'Peter',
-        email: 'paspiazusabando@gmail.com',
-        password: '123456',
-        password2: '123456'
+        name: '',
+        email: '',
+        password: '',
+        password2: ''
     });
 
     const { name, email, password, password2 } = formValues;
